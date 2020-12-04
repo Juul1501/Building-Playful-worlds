0c0557420421333338426d3189c19ad9a079cbe1
Juul Cox <36227603+Juul1501@users.noreply.github.com>
2020-12-04 16:27:03 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3;
+    public CharacterController controller;
+
+    public float mouseSensitivity = 120;
+    public GameObject camera;
+
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+    private Vector3 velocity;
+    private float angleX;
+    private float angleY;
+
+    private bool isGrounded;
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2;
+        }
+        CameraLook();
+        Move();
+    }
+
+    void CameraLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        angleX += mouseX * Time.deltaTime * mouseSensitivity;
+        angleY += mouseY * Time.deltaTime * mouseSensitivity;
+
+        Mathf.Clamp(angleY, -89f, 89f);
+
+        transform.rotation = Quaternion.Euler(0, angleX, 0);
+        camera.transform.localRotation = Quaternion.Euler(-angleY, 0, 0);
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right* x + transform.forward * z;
+        controller.Move(move* speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+        if (Input.GetButtonDown("Jump")&& isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+}
